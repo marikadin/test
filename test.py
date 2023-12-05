3341426f27ccf3605680626673a2e6bc39a340bb
itayfroo <151866243+itayfroo@users.noreply.github.com>
2023-12-05 23:26:16 +0200

    Update test.py

--- test.py
+++ test.py
@@ -7,106 +7,17 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-import tkinter as tk
 from extract_ddata import process_fin_streamers
 import sys
-from tkcalendar import Calendar
-from datetime import datetime, timedelta
-from PIL import Image, ImageTk
-
 
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-software-rasterizer')
-selected_date = ''
-app = tk.Tk()
 
-
-
-app.attributes('-fullscreen', True)
-
-app.title("Stocks")
-
-def Update():
-    try:
-        update_label()
-        app.destroy()
-    except:
-        print("You have to type stock name!")
-
-def update_search(event):
-    global keyword
-    keyword = search_text.get()
-    search_entry.delete(0, tk.END)
-    search_entry.insert(0, keyword)
-
-def open_calendar():
-    top = tk.Toplevel(app)
-
-    min_date = datetime(2022, 1, 1)
-
-    max_date = datetime.now().date()
-
-    cal = Calendar(
-        top,
-        font="Arial 14",
-        selectmode='day',
-        locale='en_US',
-        cursor="hand1",
-        year=max_date.year,
-        month=max_date.month,
-        day=max_date.day,
-        mindate=min_date,
-        maxdate=max_date,
-    )
-
-    def set_date():
-        global selected_date
-        try:
-            selected_date = str(cal.get_date())
-        except:
-            selected_date = "2022-01-01"
-        search_entry.delete(0, tk.END)
-        top.destroy()
-
-    confirm_button = tk.Button(top, text="Select Date", command=set_date)
-    confirm_button.pack(pady=10)
-    cal.pack()
-
-search_text = tk.StringVar()
-search_entry = tk.Entry(app, textvariable=search_text, width=200)
-search_entry.pack(pady=10)
-search_entry.place(relx=0.5, rely=0.3, anchor="center")
-search_entry.bind("<KeyRelease>", update_search)
-
-result_label = tk.Label(app, text="Enter the stock name: ")
-result_label.pack(pady=10)
-result_label.place(relx=0.5, rely=0.25, anchor="center")
-
-
-
-
-def update_label():
-    result_label.config(text="Search Result: " + keyword)
-
-update_button = tk.Button(app, text="Search", command=Update)
-update_button.pack(pady=10)
-update_button.place(relx=0.5, rely=0.4, anchor="center")
-
-calendar_button = tk.Button(app, text="Select Date", command=open_calendar)
-calendar_button.pack(pady=10)
-calendar_button.place(relx=0.5, rely=0.5, anchor="center")
-
-close_button = tk.Button(app, text="Close", command=app.destroy, bg='lightcoral', activebackground='red',width=20)
-close_button.pack(pady=10)
-close_button.place(relx=0.92, rely=0.02, anchor="center")
-
-app.mainloop()
-
-
+st.title("Stocks")
 def get_search_results_url(url, keyword):
-    print("Fetching...")
+    st.write("Fetching...")
     driver = webdriver.Chrome(options=chrome_options)
     driver.get(url)
 
@@ -130,60 +41,55 @@ def get_search_results_url(url, keyword):
     finally:
         driver.quit()
 
-website_url = 'https://finance.yahoo.com/'  
-try:
-    search_results_url = get_search_results_url(website_url, keyword)
-except:
-    print("Thanks for trying. \nSee u soon(:")
-    sys.exit()
-if search_results_url == "https://www.yahoo.com/?err=404&err_url=https%3A%2F%2Ffinance.yahoo.com%2Fresearch%2Freports%2FMS_0P0000061X_AnalystReport_1699903723000%3F.tsrc%3Dfin-srch":
-    print("Stock is not in Yahoo database.")
-    sys.exit(1)
-elif "https://finance.yahoo.com" not in search_results_url:
-    print("Stock is not in Yahoo database.")
-    sys.exit(1)
-elif "news" in search_results_url:
-    print("Not a stock but news: ", search_results_url)
-    sys.exit(1)
-elif "/m/" in search_results_url:
-    print("Stock is not in Yahoo database.")
-    sys.exit(1)
-elif "/company/" in search_results_url:
-    print("A private company: ", search_results_url)
-    sys.exit(1)
-else:
-    if search_results_url:
-        print(f"URL of the search results page for {keyword}: {search_results_url}")
-        # Call the second script's functionality with the obtained URL
-        from extract_ddata import process_fin_streamers
-        process_fin_streamers(keyword, search_results_url)
+# Set the minimum and maximum selectable dates
+min_date = datetime.date(2022, 1, 1)
+max_date = datetime.date.today()
+
+# Get user input for a date within the specified range, with a default value
+selected_date = st.date_input("Select a date", min_value=min_date, max_value=max_date, value=min_date)
+
+# Add a text input for stock name
+stock_name = st.text_input("Enter Stock Name")
+
+# Add a button to proceed
+if st.button("Proceed"):
+    # Check if stock name is provided
+    if not stock_name:
+        st.warning("You must enter a stock name!")
     else:
-        print(f"No search results found for {keyword}.\n")
+        st.success(f"Stock Name '{stock_name}' saved!")
 
+        # Rest of your code
+        website_url = 'https://finance.yahoo.com/'  
+        try:
+            # Combine the date and stock name for a more meaningful search
+            formatted_date = selected_date.strftime("%Y-%m-%d")
+            search_results_url = get_search_results_url(website_url, f"{stock_name} stock {formatted_date}")
+        except:
+            st.write("Thanks for trying. See you soon(:")
+            sys.exit()
 
-
-def main():
-    st.title("Choose start date")
-
-    # Set the minimum and maximum selectable dates
-    min_date = datetime.date(2022, 1, 1)
-    max_date = datetime.date.today()
-
-    # Get user input for a date within the specified range, with a default value
-    selected_date = st.date_input("Select a date", min_value=min_date, max_value=max_date, value=min_date)
-
-    # Add a text input for stock name
-    stock_name = st.text_input("Enter Stock Name")
-
-    # Add a button to save the stock name
-    if st.button("Save Stock Name"):
-        if not stock_name:
-            st.warning("You must enter a stock name!")
+        if "404" in search_results_url:
+            st.warning("Stock is not in Yahoo database.")
+            sys.exit()
+        elif "https://finance.yahoo.com" not in search_results_url:
+            st.warning("Stock is not in Yahoo database.")
+            sys.exit()
+        elif "news" in search_results_url:
+            st.warning("Not a stock but news: " + search_results_url)
+            sys.exit()
+        elif "/m/" in search_results_url:
+            st.warning("Stock is not in Yahoo database.")
+            sys.exit()
+        elif "/company/" in search_results_url:
+            st.warning("A private company: " + search_results_url)
+            sys.exit()
         else:
-            st.success(f"Stock Name '{stock_name}' saved!")
+            if search_results_url:
+                st.success(f"URL of the search results page for {stock_name}: {search_results_url}")
+                # Call the second script's functionality with the obtained URL
+                process_fin_streamers(stock_name, search_results_url)
+            else:
+                st.warning(f"No search results found for {stock_name}.")
 
-    # Display the selected date
-    st.write("You selected:", selected_date)
-
-if __name__ == "__main__":
-    main()
+# Function to get the search results URL
