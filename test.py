7f4d3df08627d2b10ac90aaeb2f14a828ccac977
itayfroo <151866243+itayfroo@users.noreply.github.com>
2023-12-06 00:56:47 +0200

    Update test.py

--- test.py
+++ test.py
@@ -38,30 +38,27 @@ stock_name = st.text_input("Enter Stock Name")
 # Function to get search results URL
 def get_search_results_url(url, keyword):
     print("Fetching...")
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--disable-software-rasterizer')
+    driver = webdriver.Chrome(options=chrome_options)
 
-    with webdriver.Chrome(options=options) as driver:
-        try:
-            driver.get(url)
+    try:
+        driver.get(url)
 
-            search_bar = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'yfin-usr-qry')))
-            search_bar.clear()
-            search_bar.send_keys(keyword)
-            search_bar.send_keys(Keys.RETURN)
+        search_bar = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'yfin-usr-qry')))
+        search_bar.clear()
+        search_bar.send_keys(keyword)
+        
+        # Use submit() to submit the form
+        search_bar.submit()
 
-            print("Waiting for search results page...")
-            WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
-            print("Search results page loaded.")
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+        time.sleep(3)
 
-            search_results_url = driver.current_url
-            return search_results_url
+        search_results_url = driver.current_url
 
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            return None
+        return search_results_url
+
+    finally:
+        driver.quit()
 
 
 
