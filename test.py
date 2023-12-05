8c364ab36347ce4e2a010c14aa65971b1cb78e2a
itayfroo <151866243+itayfroo@users.noreply.github.com>
2023-12-05 23:05:38 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import streamlit as st
 import datetime
 
 def main():
-    st.title("Streamlit Calendar Example with Default Date")
+    st.title("Choose start date")
 
     # Set the minimum and maximum selectable dates
     min_date = datetime.date(2022, 1, 1)
@@ -10,6 +10,16 @@ def main():
 
     # Get user input for a date within the specified range, with a default value
     selected_date = st.date_input("Select a date", min_value=min_date, max_value=max_date, value=min_date)
+
+    # Add a text input for stock name
+    stock_name = st.text_input("Enter Stock Name")
+
+    # Add a button to save the stock name
+    if st.button("Save Stock Name"):
+        if not stock_name:
+            st.warning("You must enter a stock name!")
+        else:
+            st.success(f"Stock Name '{stock_name}' saved!")
 
     # Display the selected date
     st.write("You selected:", selected_date)
