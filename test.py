a8ab98c8a2fbdd1e11538fad85cbc063bf1c88f1
itayfroo <151866243+itayfroo@users.noreply.github.com>
2023-12-05 23:02:27 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,10 +2,14 @@ import streamlit as st
 import datetime
 
 def main():
-    st.title("Streamlit Calendar Example")
+    st.title("Streamlit Calendar Example with Default Date")
 
-    # Get user input for a date using date_input
-    selected_date = st.date_input("Select a date", datetime.date.today())
+    # Set the minimum and maximum selectable dates
+    min_date = datetime.date(2022, 1, 1)
+    max_date = datetime.date.today()
+
+    # Get user input for a date within the specified range, with a default value
+    selected_date = st.date_input("Select a date", min_value=min_date, max_value=max_date, value=min_date)
 
     # Display the selected date
     st.write("You selected:", selected_date)
