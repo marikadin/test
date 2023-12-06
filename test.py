968c5d518151fdca31d716a2494db55e8112d608
itayfroo <151866243+itayfroo@users.noreply.github.com>
2023-12-06 23:08:08 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 import streamlit as st
-#from extract_ddata import process_fin_streamers
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -8,21 +7,18 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 from datetime import datetime
+
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-software-rasterizer')
-chrome_options.binary_location = 'C:\Program Files\Google\Chrome'
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--disable-software-rasterizer')
-chrome_options.binary_location = 'C:\Program Files\Google\Chrome' 
+chrome_options.binary_location = 'C:\Program Files\Google\Chrome\Application\chrome.exe'  
+chrome_driver_path = 'C:\Program Files\Google\Chrome\Application'  
+
 def get_search_results_url(url, keyword):
     st.write("Fetching...")
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.get(url)
-
+    driver = webdriver.Chrome(executable_path=chrome_driver_path, options=chrome_options)
+    
     try:
         search_bar = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'yfin-usr-qry')))
         search_bar.clear()
@@ -61,19 +57,20 @@ if st.button("Search"):
 
         # Your logic for processing the data, use keyword and selected_date as needed
         website_url = 'https://finance.yahoo.com/'
-        #try:
-        search_results_url = get_search_results_url(website_url, keyword)
-        print(search_results_url)
-        #except:
-            #st.error("An error occurred while fetching data.")
-            #st.stop()
+        try:
+            search_results_url = get_search_results_url(website_url, keyword)
+            print(search_results_url)
+        except:
+            st.error("An error occurred while fetching data.")
+            st.stop()
 
         if search_results_url:
             st.write(f"URL of the search results page for {keyword}: {search_results_url}")
             # Call the second script's functionality with the obtained URL
-            #process_fin_streamers(keyword, search_results_url)
+            # process_fin_streamers(keyword, search_results_url)
         else:
             st.warning(f"No search results found for {keyword}.")
 
+# Close Button
 if st.button("Close"):
     st.stop()  # Stops the Streamlit app
