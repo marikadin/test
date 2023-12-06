f53268e79879556b95a2951749bfb94272c5146b
itayfroo <151866243+itayfroo@users.noreply.github.com>
2023-12-07 00:01:49 +0200

    Update test.py

--- test.py
+++ test.py
@@ -23,34 +23,34 @@ def get_search_results_url(url, keyword):
 
 website_url = 'https://finance.yahoo.com/'  
 keyword = st.text_input("Enter the stock name:")
+if st.button("Enter"):
+    try:
+        search_results_url = get_search_results_url(website_url, keyword)
+    except:
+        st.write("Thanks for trying. \nSee u soon(:")
+        sys.exit()
 
-try:
-    search_results_url = get_search_results_url(website_url, keyword)
-except:
-    st.write("Thanks for trying. \nSee u soon(:")
-    sys.exit()
-
-# Rest of the conditions based on the original code
-if search_results_url == "https://www.yahoo.com/?err=404&err_url=https%3A%2F%2Ffinance.yahoo.com%2Fresearch%2Freports%2FMS_0P0000061X_AnalystReport_1699903723000%3F.tsrc%3Dfin-srch":
-    st.write("Stock is not in Yahoo database.")
-    sys.exit(1)
-elif "https://finance.yahoo.com" not in search_results_url:
-    st.write("Stock is not in Yahoo database.")
-    sys.exit(1)
-elif "news" in search_results_url:
-    st.write("Not a stock but news: ", search_results_url)
-    sys.exit(1)
-elif "/m/" in search_results_url:
-    st.write("Stock is not in Yahoo database.")
-    sys.exit(1)
-elif "/company/" in search_results_url:
-    st.write("A private company: ", search_results_url)
-    sys.exit(1)
-else:
-    if search_results_url:
-        st.write(f"URL of the search results page for {keyword}: {search_results_url}")
-        # Call the second script's functionality with the obtained URL
-        # from extract_ddata import process_fin_streamers
-        # process_fin_streamers(keyword, search_results_url)
+    # Rest of the conditions based on the original code
+    if search_results_url == "https://www.yahoo.com/?err=404&err_url=https%3A%2F%2Ffinance.yahoo.com%2Fresearch%2Freports%2FMS_0P0000061X_AnalystReport_1699903723000%3F.tsrc%3Dfin-srch":
+        st.write("Stock is not in Yahoo database.")
+        sys.exit(1)
+    elif "https://finance.yahoo.com" not in search_results_url:
+        st.write("Stock is not in Yahoo database.")
+        sys.exit(1)
+    elif "news" in search_results_url:
+        st.write("Not a stock but news: ", search_results_url)
+        sys.exit(1)
+    elif "/m/" in search_results_url:
+        st.write("Stock is not in Yahoo database.")
+        sys.exit(1)
+    elif "/company/" in search_results_url:
+        st.write("A private company: ", search_results_url)
+        sys.exit(1)
     else:
-        st.write(f"No search results found for {keyword}.\n")
+        if search_results_url:
+            st.write(f"URL of the search results page for {keyword}: {search_results_url}")
+            # Call the second script's functionality with the obtained URL
+            # from extract_ddata import process_fin_streamers
+            # process_fin_streamers(keyword, search_results_url)
+        else:
+            st.write(f"No search results found for {keyword}.\n")
