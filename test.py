ce552b55ae4e287f4bcb569df9c34f8189eb47ec
itayfroo <151866243+itayfroo@users.noreply.github.com>
2023-12-06 23:46:09 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import subprocess
 
-keyword = st.text_input()
+keyword = st.text_input("Enter keyword")
 if st.button("Submit"):
     st.write("You entered:", keyword)
     import test
