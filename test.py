c34fed8a9428e1a4876ab1a827184cff561c7b02
itayfroo <151866243+itayfroo@users.noreply.github.com>
2023-12-07 00:04:53 +0200

    s

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import webbrowser
 import time
 import streamlit as st
 import sys
+from bs4 import BeautifulSoup
+import requests
 def get_search_results_url(url, keyword):
     st.write("Fetching...")
 
@@ -9,17 +11,32 @@ def get_search_results_url(url, keyword):
         # Construct the search URL
         search_url = f"{url}/quote/{keyword}"
 
-        # Open the search URL in the default web browser
-        webbrowser.open_new_tab(search_url)
+        # Use requests to get the HTML content of the page
+        response = requests.get(search_url)
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-        # Sleep for 3 seconds (similar to time.sleep(3) in your original code)
-        time.sleep(3)
+        # Extract the URL of the first search result
+        first_result = soup.find('a', {'data-test': 'search-link'})
+        if first_result:
+            first_result_url = f"{url}{first_result['href']}"
+            
+            # Open the first result URL in the default web browser
+            webbrowser.open_new_tab(first_result_url)
 
-        # Return the search URL (optional, you can modify this based on your needs)
-        return search_url
-    except:
-        st.write("Failed fetching")
+            # Return the first result URL
+            return first_result_url
+        else:
+            st.write("No search results found.")
+            return None
+
+    except Exception as e:
+        st.write(f"Failed fetching: {e}")
         return None
+
+website_url = 'https://finance.yahoo.com/'  
+keyword = st.text_input("Enter the stock name:")
+
+
 
 website_url = 'https://finance.yahoo.com/'  
 keyword = st.text_input("Enter the stock name:")
