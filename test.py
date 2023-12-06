4d45a0b6e23022ad71a80934a181690d661a98fd
itayfroo <151866243+itayfroo@users.noreply.github.com>
2023-12-06 23:13:52 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,13 @@
 import streamlit as st
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
-from datetime import datetime
 
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-software-rasterizer')
-chrome_options.binary_location = 'C:\Program Files\Google\Chrome\Application\chrome.exe'  
-chrome_driver_path = 'C:\Program Files\Google\Chrome\Application'  
+chrome_options.binary_location = 'C:\Program Files\Google\Chrome\Application\chrome.exe'  # Adjust this path
+chrome_driver_path = 'C:\Program Files\Google\Chrome\Application'  # Adjust this path
 
 def get_chromedriver_version(chrome_driver_path):
     try:
@@ -25,68 +19,6 @@ def get_chromedriver_version(chrome_driver_path):
     except Exception as e:
         return f"Error getting ChromeDriver version: {e}"
 
-def get_search_results_url(url, keyword):
-    st.write("Fetching...")
-    
-    # Print ChromeDriver version for debugging
-    chrome_driver_version = get_chromedriver_version(chrome_driver_path)
-    st.write(f"ChromeDriver version: {chrome_driver_version}")
-
-    driver = webdriver.Chrome(executable_path=chrome_driver_path, options=chrome_options)
-    
-    try:
-        search_bar = driver.find_element_by_id('yfin-usr-qry')  # Using find_element_by_id directly
-        search_bar.clear()
-        search_bar.send_keys(keyword)
-        search_bar.send_keys(Keys.RETURN)
-
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
-        time.sleep(3)
-
-        search_results_url = driver.current_url
-
-        return search_results_url
-
-    finally:
-        driver.quit()
-
-
-# Set up Streamlit page
-st.set_page_config(page_title="Stocks", page_icon=None, layout='wide')
-
-# Title
-st.title("Stocks")
-
-# User Input for Stock Name
-keyword = st.text_input("Enter the stock name:")
-
-# Streamlit Calendar
-selected_date = st.date_input("Select Date", min_value=datetime(2022, 1, 1), max_value=datetime.now())
-
-# Button to Trigger Update
-if st.button("Search"):
-    if not keyword:
-        st.error("Please enter a stock name.")
-    else:
-        # Process the stock data
-        st.write(f"Search Result: {keyword}")
-
-        # Your logic for processing the data, use keyword and selected_date as needed
-        website_url = 'https://finance.yahoo.com/'
-        #try:
-        search_results_url = get_search_results_url(website_url, keyword)
-        print(search_results_url)
-        #except:
-            #st.error("An error occurred while fetching data.")
-            #st.stop()
-
-        if search_results_url:
-            st.write(f"URL of the search results page for {keyword}: {search_results_url}")
-            # Call the second script's functionality with the obtained URL
-            # process_fin_streamers(keyword, search_results_url)
-        else:
-            st.warning(f"No search results found for {keyword}.")
-
-# Close Button
-if st.button("Close"):
-    st.stop()  # Stops the Streamlit app
+# Print ChromeDriver version for debugging
+chrome_driver_version = get_chromedriver_version(chrome_driver_path)
+st.write(f"ChromeDriver version: {chrome_driver_version}")
