94e5576b012ea45c11e1aa7dd66abd9640012b9b
itayfroo <151866243+itayfroo@users.noreply.github.com>
2023-12-06 23:19:08 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,24 +1,73 @@
 import streamlit as st
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-software-rasterizer')
-chrome_options.binary_location = 'C:\Program Files\Google\Chrome\Application\chrome.exe'  # Adjust this path
-chrome_driver_path = 'C:\Program Files\Google'  # Adjust this path
 
-def get_chromedriver_version(chrome_driver_path):
+def get_search_results_url(keyword):
+    website_url = 'https://finance.yahoo.com/'
+
+    print("Fetching...")
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(website_url)
+
     try:
-        # Run ChromeDriver with the --version flag
-        import subprocess
-        result = subprocess.run([chrome_driver_path, '--version'], capture_output=True, text=True)
-        chromedriver_version = result.stdout.strip()
-        return chromedriver_version
-    except Exception as e:
-        return f"Error getting ChromeDriver version: {e}"
+        search_bar = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'yfin-usr-qry')))
+        search_bar.clear()
+        search_bar.send_keys(keyword)
+        search_bar.send_keys(Keys.RETURN)
 
-# Print ChromeDriver version for debugging
-chrome_driver_version = get_chromedriver_version(chrome_driver_path)
-st.write(f"ChromeDriver version: {chrome_driver_version}")
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+        time.sleep(3)
+
+        search_results_url = driver.current_url
+
+        # Check if the URL is valid
+        if "https://finance.yahoo.com" not in search_results_url:
+            return None  # Invalid URL
+
+        # Check for specific conditions
+        if "news" in search_results_url:
+            return None  # Not a stock, but news
+
+        return search_results_url
+
+    finally:
+        driver.quit()
+
+# Set up Streamlit page
+st.set_page_config(page_title="Stocks", page_icon=None, layout='wide')
+
+# Title
+st.title("Stocks")
+
+# User Input for Stock Name
+keyword = st.text_input("Enter the stock name:")
+
+# Button to Trigger Search
+if st.button("Search"):
+    if not keyword:
+        st.error("Please enter a stock name.")
+    else:
+        # Process the stock data
+        st.write(f"Searching for: {keyword}")
+
+        # Fetch search results URL using Selenium
+        search_results_url = get_search_results_url(keyword)
+
+        if search_results_url:
+            st.write(f"Search results URL: {search_results_url}")
+        else:
+            st.warning(f"No valid search results found for {keyword}.")
+
+# Close Button
+if st.button("Close"):
+    st.stop()  # Stops the Streamlit app
