5d05262b1094a6595b97d0d33d22789a6cabbb83
itayfroo <151866243+itayfroo@users.noreply.github.com>
2023-12-06 23:23:39 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-import streamlit as st
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -6,68 +5,75 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import tkinter as tk
+#from extract_ddata import process_fin_streamers
+import sys
+#from tkcalendar import Calendar
+#from datetime import datetime, timedelta
+#from PIL import Image, ImageTk
+import streamlit as st
 
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-software-rasterizer')
 
-def get_search_results_url(keyword):
-    website_url = 'https://finance.yahoo.com/'
+keyword = st.text_input("")
 
+
+def get_search_results_url(url, keyword):
     print("Fetching...")
     driver = webdriver.Chrome(options=chrome_options)
-    driver.get(website_url)
+    driver.get(url)
 
     try:
         search_bar = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'yfin-usr-qry')))
+
         search_bar.clear()
+
         search_bar.send_keys(keyword)
+
         search_bar.send_keys(Keys.RETURN)
 
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+
         time.sleep(3)
 
         search_results_url = driver.current_url
-
-        # Check if the URL is valid
-        if "https://finance.yahoo.com" not in search_results_url:
-            return None  # Invalid URL
-
-        # Check for specific conditions
-        if "news" in search_results_url:
-            return None  # Not a stock, but news
 
         return search_results_url
 
     finally:
         driver.quit()
 
-# Set up Streamlit page
-st.set_page_config(page_title="Stocks", page_icon=None, layout='wide')
-
-# Title
-st.title("Stocks")
-
-# User Input for Stock Name
-keyword = st.text_input("Enter the stock name:")
-
-# Button to Trigger Search
-if st.button("Search"):
-    if not keyword:
-        st.error("Please enter a stock name.")
+website_url = 'https://finance.yahoo.com/'  
+try:
+    search_results_url = get_search_results_url(website_url, keyword)
+except:
+    print("Thanks for trying. \nSee u soon(:")
+    sys.exit()
+if search_results_url == "https://www.yahoo.com/?err=404&err_url=https%3A%2F%2Ffinance.yahoo.com%2Fresearch%2Freports%2FMS_0P0000061X_AnalystReport_1699903723000%3F.tsrc%3Dfin-srch":
+    print("Stock is not in Yahoo database.")
+    sys.exit(1)
+elif "https://finance.yahoo.com" not in search_results_url:
+    print("Stock is not in Yahoo database.")
+    sys.exit(1)
+elif "news" in search_results_url:
+    print("Not a stock but news: ", search_results_url)
+    sys.exit(1)
+elif "/m/" in search_results_url:
+    print("Stock is not in Yahoo database.")
+    sys.exit(1)
+elif "/company/" in search_results_url:
+    print("A private company: ", search_results_url)
+    sys.exit(1)
+else:
+    if search_results_url:
+        print(f"URL of the search results page for {keyword}: {search_results_url}")
+        # Call the second script's functionality with the obtained URL
+        #from extract_ddata import process_fin_streamers
+        #process_fin_streamers(keyword, search_results_url)
     else:
-        # Process the stock data
-        st.write(f"Searching for: {keyword}")
+        print(f"No search results found for {keyword}.\n")
 
-        # Fetch search results URL using Selenium
-        search_results_url = get_search_results_url(keyword)
 
-        if search_results_url:
-            st.write(f"Search results URL: {search_results_url}")
-        else:
-            st.warning(f"No valid search results found for {keyword}.")
-
-# Close Button
-if st.button("Close"):
-    st.stop()  # Stops the Streamlit app
