aac08bf1a2b85c6354f2fc74d06a421c97186a76
itayfroo <151866243+itayfroo@users.noreply.github.com>
2023-12-06 23:30:13 +0200

    Update test.py

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-software-rasterizer')
 
-keyword = st.text_input("Enter a string:", "Default value")
+keyword = st.text_input("Enter a string:")
 st.write("You entered:", keyword)
 
 
