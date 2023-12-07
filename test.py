d34746f8f3688798585341f30057842d98ae4266
itayfroo <151866243+itayfroo@users.noreply.github.com>
2023-12-07 23:32:20 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,9 @@ import yfinance as yf
 import pandas as pd
 import plotly.express as px
 from sklearn.linear_model import LinearRegression
-from datetime import timedelta
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+import tensorflow as tf
 
 def get_stock_data(symbol):
     try:
@@ -45,7 +47,7 @@ def get_stock_symbol(api_key, company_name):
         st.error(f"Error: {e}")
         return None
 
-def predict_tomorrows_stock_value(stock_data):
+def predict_tomorrows_stock_value_linear_regression(stock_data):
     # Assuming a simple linear regression model for demonstration
     X = pd.DataFrame({'Days': range(1, len(stock_data) + 1)})
     y = stock_data['Close']
@@ -56,6 +58,44 @@ def predict_tomorrows_stock_value(stock_data):
     # Predict tomorrow's stock value
     tomorrow = X.iloc[[-1]]['Days'].values[0] + 1
     predicted_value = model.predict([[tomorrow]])[0]
+
+    return predicted_value
+
+def predict_tomorrows_stock_value_lstm(stock_data):
+    # Normalize the data
+    scaler = MinMaxScaler()
+    data_normalized = scaler.fit_transform(stock_data['Close'].values.reshape(-1, 1))
+
+    # Create sequences for LSTM
+    seq_length = 10
+    sequences, labels = [], []
+    for i in range(len(data_normalized) - seq_length):
+        seq = data_normalized[i:i + seq_length]
+        label = data_normalized[i + seq_length]
+        sequences.append(seq)
+        labels.append(label)
+
+    X_train, y_train = np.array(sequences), np.array(labels)
+    X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], 1))
+
+    # Build LSTM model
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.LSTM(50, activation='relu', input_shape=(seq_length, 1)),
+        tf.keras.layers.Dense(1)
+    ])
+
+    model.compile(optimizer='adam', loss='mse')
+
+    # Train the model
+    model.fit(X_train, y_train, epochs=50, batch_size=32)
+
+    # Generate a new sequence for prediction
+    last_sequence = data_normalized[-seq_length:]
+    last_sequence = last_sequence.reshape((1, seq_length, 1))
+
+    # Predict tomorrow's stock value using LSTM
+    predicted_value = model.predict(last_sequence)
+    predicted_value = scaler.inverse_transform(predicted_value.reshape(1, -1))[0, 0]
 
     return predicted_value
 
@@ -86,9 +126,12 @@ if st.button("Get Stock Symbol"):
                 if stock_data is not None:
                     plot_stock_data(stock_data)
 
-                    # Predict tomorrow's stock value
-                    predicted_value = predict_tomorrows_stock_value(stock_data)
+                    # Predict tomorrow's stock value using Linear Regression
+                    predicted_value_lr = predict_tomorrows_stock_value_linear_regression(stock_data)
+                    st.write(f"Approximate tomorrow's stock value (Linear Regression): ${predicted_value_lr:.2f}")
 
-                    st.write(f"Approximate tomorrow's stock value: ${predicted_value:.2f}")
+                    # Predict tomorrow's stock value using LSTM
+                    predicted_value_lstm = predict_tomorrows_stock_value_lstm(stock_data)
+                    st.write(f"Approximate tomorrow's stock value (LSTM): ${predicted_value_lstm:.2f}")
         else:
             st.warning(f"Could not find the stock symbol for {company_name}")
