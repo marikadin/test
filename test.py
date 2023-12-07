8e3b92c53ec71c37c44dc89f3bb05cf3b0eeb670
itayfroo <151866243+itayfroo@users.noreply.github.com>
2023-12-07 22:45:36 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import streamlit as st
+from streamlit.components.v1 import html
 
 def open_website(url):
     iframe_code = f'<iframe src="{url}" width="100%" height="600" frameborder="0" scrolling="yes"></iframe>'
-    st.markdown(iframe_code, unsafe_allow_html=True)
+    html(iframe_code, height=600)
 
 st.title("Website Viewer")
 
