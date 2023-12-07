1beb5eb37e928b69de8259143de6a709611df8ad
itayfroo <151866243+itayfroo@users.noreply.github.com>
2023-12-07 23:27:07 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,9 @@ import requests
 import yfinance as yf
 import pandas as pd
 import plotly.express as px
+from sklearn.linear_model import LinearRegression
+from datetime import timedelta
+
 def get_stock_data(symbol):
     try:
         stock_data = yf.download(symbol, start="2022-01-01", end="2023-01-01")
@@ -10,15 +13,17 @@ def get_stock_data(symbol):
     except Exception as e:
         st.error(f"Error retrieving data: {e}")
         return None
+
 def plot_stock_data(stock_data):
     fig = px.line(stock_data, x=stock_data.index, y='Close', title='Stock Prices Over the Last Year')
     fig.update_xaxes(title_text='Date')
     fig.update_yaxes(title_text='Stock Price (USD)')
     st.plotly_chart(fig)
+
 def get_stock_symbol(api_key, company_name):
     base_url = "https://www.alphavantage.co/query"
     function = "SYMBOL_SEARCH"
-    
+
     # Make the API request
     params = {
         "function": function,
@@ -40,8 +45,22 @@ def get_stock_symbol(api_key, company_name):
         st.error(f"Error: {e}")
         return None
 
+def predict_tomorrows_stock_value(stock_data):
+    # Assuming a simple linear regression model for demonstration
+    X = pd.DataFrame({'Days': range(1, len(stock_data) + 1)})
+    y = stock_data['Close']
+
+    model = LinearRegression()
+    model.fit(X, y)
+
+    # Predict tomorrow's stock value
+    tomorrow = X.iloc[[-1]]['Days'].values[0] + 1
+    predicted_value = model.predict([[tomorrow]])[0]
+
+    return predicted_value
+
 # Streamlit app
-st.title("Stock Symbol Lookup")
+st.title("Stock Symbol Lookup and Prediction")
 
 # Input for company name
 company_name = st.text_input("Enter company name or item:")
@@ -60,7 +79,6 @@ if st.button("Get Stock Symbol"):
         if stock_symbol:
             st.title("Stock Price Visualization App")
 
-
             if stock_symbol:
                 st.write(f"Displaying stock data for {stock_symbol}")
 
@@ -68,5 +86,9 @@ if st.button("Get Stock Symbol"):
                 if stock_data is not None:
                     plot_stock_data(stock_data)
 
+                    # Predict tomorrow's stock value
+                    predicted_value = predict_tomorrows_stock_value(stock_data)
+
+                    st.write(f"Approximate tomorrow's stock value: ${predicted_value:.2f}")
         else:
             st.warning(f"Could not find the stock symbol for {company_name}")
