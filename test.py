8c2ae49a484b2473ccade811a6f56399542f629b
itayfroo <151866243+itayfroo@users.noreply.github.com>
2023-12-07 23:12:31 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,7 @@
 import streamlit as st
-import yahooquery as yq
 import yfinance as yf
 import pandas as pd
 import plotly.express as px
-
-def get_stock_symbol(company_name):
-    try:
-        response = yq.search(company_name)
-        st.warning("Response:", response)  # Print the entire response
-        if response and 'quotes' in response and response['quotes']:
-            return response['quotes'][0]['symbol']
-        else:
-            st.error(f"Could not find stock symbol for {company_name}")
-            return None
-    except Exception as e:
-        st.error(f"Error retrieving stock symbol: {e}")
-        return None
 
 def get_stock_data(symbol):
     try:
@@ -34,16 +20,14 @@ def plot_stock_data(stock_data):
 def main():
     st.title("Stock Price Visualization App")
 
-    company_name = st.text_input("Enter the company name (e.g., Nvidia, Apple):")
+    stock_name = st.text_input("Enter the stock symbol or company name (e.g., NVDA, AAPL):")
 
-    if company_name:
-        st.write(f"Displaying stock data for {company_name}")
+    if stock_name:
+        st.write(f"Displaying stock data for {stock_name}")
 
-        stock_symbol = get_stock_symbol(company_name)
-        if stock_symbol:
-            stock_data = get_stock_data(stock_symbol)
-            if stock_data is not None:
-                plot_stock_data(stock_data)
+        stock_data = get_stock_data(stock_name)
+        if stock_data is not None:
+            plot_stock_data(stock_data)
 
 if __name__ == "__main__":
     main()
