264c8202e27ad37ca2958d6dd0a1329f40b866a1
itayfroo <151866243+itayfroo@users.noreply.github.com>
2023-12-07 23:21:47 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,20 @@
 import streamlit as st
 import requests
-
+import yfinance as yf
+import pandas as pd
+import plotly.express as px
+def get_stock_data(symbol):
+    try:
+        stock_data = yf.download(symbol, start="2022-01-01", end="2023-01-01")
+        return stock_data
+    except Exception as e:
+        st.error(f"Error retrieving data: {e}")
+        return None
+def plot_stock_data(stock_data):
+    fig = px.line(stock_data, x=stock_data.index, y='Close', title='Stock Prices Over the Last Year')
+    fig.update_xaxes(title_text='Date')
+    fig.update_yaxes(title_text='Stock Price (USD)')
+    st.plotly_chart(fig)
 def get_stock_symbol(api_key, company_name):
     base_url = "https://www.alphavantage.co/query"
     function = "SYMBOL_SEARCH"
@@ -44,6 +58,16 @@ if st.button("Get Stock Symbol"):
     else:
         stock_symbol = get_stock_symbol(api_key, company_name)
         if stock_symbol:
-            st.success(f"The stock symbol for {company_name} is {stock_symbol}")
+            def main():
+                st.title("Stock Price Visualization App")
+                global stock_symbol
+                if stock_symbol:
+                    st.write(f"Displaying stock data for {stock_symbol}")
+
+                    stock_data = get_stock_data(stock_symbol)
+                    if stock_data is not None:
+                        plot_stock_data(stock_data)
         else:
             st.warning(f"Could not find the stock symbol for {company_name}")
+if __name__ == "__main__":
+    main()
