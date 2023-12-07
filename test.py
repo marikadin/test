e642ac76ebcfc7d440fce9244a6929c8f4f1356a
itayfroo <151866243+itayfroo@users.noreply.github.com>
2023-12-07 14:13:06 +0200

    [

--- test.py
+++ test.py
@@ -6,7 +6,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-from tkcalendar import Calendar
 from datetime import datetime
 
 chrome_options = Options()
