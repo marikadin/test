0e2acf8c6a379f2a65711966a6e8d7a3e6914c17
itayfroo <151866243+itayfroo@users.noreply.github.com>
2023-12-07 22:47:09 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ import webbrowser
 
 def open_website(url):
     webbrowser.open(url, new=2)
+    iframe_code = f'<iframe src="{url}" width="100%" height="600" frameborder="0" scrolling="yes"></iframe>'
+    st.markdown(iframe_code, unsafe_allow_html=True)
 
 st.title("Website Viewer")
 
