2ad012ac600ce0dc460b2829d7052728b082ac7e
itayfroo <151866243+itayfroo@users.noreply.github.com>
2023-12-08 17:34:50 +0200

    Update test.py

--- test.py
+++ test.py
@@ -156,16 +156,6 @@ st.set_page_config(
     layout="wide",
 
 )
-background_image_style = """
-<style>
-body {
-    background-image: url(r'C:/Users/user/Documents/test/background.jpg');  
-    background-size: cover;
-}
-</style>
-"""
-
-st.markdown(background_image_style, unsafe_allow_html=True)
 
 st.title("Stock Analyzer")
 
