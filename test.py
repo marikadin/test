01e87b9947611730f0a564463c54c1b43dd40593
itayfroo <151866243+itayfroo@users.noreply.github.com>
2023-12-08 17:08:20 +0200

    Update test.py

--- test.py
+++ test.py
@@ -152,36 +152,7 @@ st.set_page_config(
     layout="wide",
 )
 st.title("Stock Analyzer")
-# Input for company name
-video_html = """
-		<style>
 
-		#myVideo {
-		  position: fixed;
-		  right: 0;
-		  bottom: 0;
-		  min-width: 100%; 
-		  min-height: 100%;
-		}
-
-		.content {
-		  position: fixed;
-		  bottom: 0;
-		  background: rgba(0, 0, 0, 0.5);
-		  color: #f1f1f1;
-		  width: 100%;
-		  padding: 20px;
-		}
-
-		</style>	
-		<video autoplay muted loop id="myVideo">
-		  <video controls>
-            <source type="video/mp4" 
-            src="C:\Users\user\Documents\test\video.mp4">
-		</video>
-        """
-
-st.markdown(video_html, unsafe_allow_html=True)
 company_name = st.text_input("Enter company name or item:")
 
 if st.button("Get Stock Symbol"):
