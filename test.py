c8a6b3cbe7193a3fae32b750caa4dd045e308409
marikadin <153016365+marikadin@users.noreply.github.com>
2023-12-23 10:30:09 +0200

    Update test.py

--- test.py
+++ test.py
@@ -32,7 +32,7 @@ def main():
     elif page == "Stock Analysis":
         show_stock_analysis_page()
 def show_home_page():
-    st.image("your_logo.png", use_column_width=True)  # Replace "your_logo.png" with your logo file
+    st.image("icons8-stock-48.png", use_column_width=True)  # Replace "your_logo.png" with your logo file
     st.write("Welcome to the Stock Analyzer app!")
     st.write("Choose 'Stock Analysis' from the sidebar to start analyzing stocks.")
 def show_stock_analysis_page():
