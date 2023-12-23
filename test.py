fa8a28587169fa59b2dbc9633d9e5eb55885cb66
marikadin <3007kol@gmail.com>
2023-12-23 11:36:26 +0200

    Update test.py

--- test.py
+++ test.py
@@ -273,11 +273,11 @@ def get_stock_symbol(company_name):
             st.error(f"Error: {e}")
 
     return None
-    def get_stock_data(symbol, start_date, end_date):
-        try:
-            stock_data = yf.download(symbol, start=start_date, end=end_date)
-            return stock_data
-        except Exception as e:
-            st.error(f"Error retrieving data: {e}")
-            return None
+def get_stock_data(symbol, start_date, end_date):
+    try:
+        stock_data = yf.download(symbol, start=start_date, end=end_date)
+        return stock_data
+    except Exception as e:
+        st.error(f"Error retrieving data: {e}")
+        return None
 main()
