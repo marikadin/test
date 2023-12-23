3844b247e90b3c7ca250be5ef90538c45621cf7f
marikadin <3007kol@gmail.com>
2023-12-23 16:00:33 +0200

    Update test.py

--- test.py
+++ test.py
@@ -39,62 +39,50 @@ def show_home_page():
     st.write("Choose 'Stock Analysis' from the sidebar to start analyzing stocks.")
 
 
-
 def show_real_time_investment_page():
+    st.title("Real-time stock price change")
+    company_name = st.text_input("Enter company name or item:")
+    money_invested = st.number_input("How much money did you invest", value=0, step=1, key="money_invested_key")
+    start_date = st.date_input("Select start date", datetime.date(2022, 1, 1))
+    end_date = datetime.datetime.now().date()
+
+    if st.button("Get Stock Symbol"):
+        if company_name == "" or money_invested is None:
+            st.warning("You have to enter a stock and a money amount.")
+        else:
+            stock_symbol = get_stock_symbol(company_name)
+            if stock_symbol:
+                st.title("Stock Price Visualization App")
+                st.write(f"Displaying stock data for {stock_symbol}")
+
+                stock_data, start_price, last_price = get_stock_data(stock_symbol, start_date, end_date)
+                if stock_data is not None:
+                    percent_change = ((last_price - start_price) / abs(start_price)) * 100
+                    changed_money = money_invested + (money_invested * (percent_change / 100))
+                    st.write(f"Money invested: ${money_invested:.2f}")
+                    st.write(f"Invested money today: ${changed_money:.2f}")
+                    Money_list.append(money_invested)
+                    New_Money_list.append(changed_money)
+                    Name_list.append(stock_symbol)
+
+                            
+def all_investments():
     global button_pressed, Money_list, New_Money_list, Name_list
 
-    if not button_pressed:
-        all_investments()
+    button_placeholder = st.empty()
+
+    if button_placeholder.button("Add investment", key="add_investment"):
+        button_pressed = True
+        show_real_time_investment_page()
+
+    if not Money_list:
+        st.write("You don't have any investments")
     else:
-        st.title("Real time stock price change")
-        company_name = st.text_input("Enter company name or item:")
-        money_invested = st.number_input("How much money did you invest", value=0, step=1,
-                                         key="money_invested_key_button")  # Modified key
-        start_date = st.date_input("Select start date", datetime.date(2022, 1, 1))
-        end_date = datetime.datetime.now().date()
+        for i in range(len(Money_list)):
+            st.write(f"Invested money: {Money_list[i]}\nInvested money today: {New_Money_list[i]}\nProfit: {New_Money_list[i] - Money_list[i]}")
 
-        money_invested_value = st.empty()
-
-        if st.button("Get Stock Symbol"):
-            if company_name == "" or money_invested is None:
-                st.warning("You have to enter a stock and a money amount.")
-            else:
-                stock_symbol = get_stock_symbol(company_name)
-                if stock_symbol:
-                    st.title("Stock Price Visualization App")
-                    st.write(f"Displaying stock data for {stock_symbol}")
-
-                    stock_data, start_price, last_price = get_stock_data(stock_symbol, start_date, end_date)
-                    if stock_data is not None:
-                        percent_change = ((last_price - start_price) / abs(start_price)) * 100
-                        changed_money = money_invested + (money_invested * (percent_change / 100))
-                        st.write(f"Money invested: ${money_invested:.2f}")
-                        st.write(f"Invested money today: ${changed_money:.2f}")
-                        Money_list.append(money_invested)
-                        New_Money_list.append(changed_money)
-                        Name_list.append(stock_symbol)
-                        button_pressed = False
-                        all_investments()
-
-        # Check if the value of the number input has changed before executing all_investments
-        if money_invested != Money_list[-1]:
-            money_invested_value.number_input("How much money did you invest", value=money_invested, step=1,
-                                             key="money_invested_key_button")  # Modified key
-
-
-    def all_investments():
-        global button_pressed
-
-        if st.button("Add investment", key="add_investment_button"):
-            button_pressed = True
-            show_real_time_investment_page()
-
-        if not Money_list:
-            st.write("You don't have any investments")
-        else:
-            for i in range(len(Money_list)):
-                st.write(f"Invested money: {Money_list[i]}\nInvested money today: {New_Money_list[i]}\nProfit: {New_Money_list[i] - Money_list[i]}")
-
+    if button_pressed:
+        button_placeholder.empty()
 
 def get_stock_symbol(company_name):
     for _ in range(len(api_keys)):
