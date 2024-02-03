dbea5ebed4f81d0e2eceda2bdffe84921fb27f75
marikadin <3007kol@gmail.com>
2024-02-03 18:18:20 +0200

    Update test.py

--- test.py
+++ test.py
@@ -369,13 +369,14 @@ def homepage():
 
 
 def language_chooser():
-    if 'chosen_language' not in st.session_state:
-        st.session_state.chosen_language = 'en'  
+    if 'chosen_language' not in st.session:
+        st.session['chosen_language'] = 'en'
 
     st.header(print_word("Choose a language"))
     language_options = ['Russian', 'English', 'Hebrew']
-    st.session_state.chosen_language = st.selectbox("Choose a language", language_options)
-    st.session_state.chosen_language = st.session_state.chosen_language[:2].lower()
+    st.session['chosen_language'] = st.selectbox("Choose a language", language_options)
+    st.session['chosen_language'] = st.session['chosen_language'][:2].lower()
+
 
 
 
@@ -387,8 +388,8 @@ def translate_word(word, chosen_language):
     return translated_word
 
 def print_word(word):
-    if 'chosen_language' in st.session_state:
-        translated_word = translate_word(word, st.session_state['chosen_language'])
+    if 'chosen_language' in st.session:
+        translated_word = translate_word(word, st.session['chosen_language'])
         return translated_word
 
 
