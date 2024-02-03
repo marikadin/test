52e1f4b2a5ec7472b79c77bee49202c5f844caf0
marikadin <3007kol@gmail.com>
2024-02-03 19:23:35 +0200

    Update test.py

--- test.py
+++ test.py
@@ -372,7 +372,7 @@ def language_chooser():
     if 'chosen_language' not in st.session_state:
         st.session_state.chosen_language = 'en'
 
-    st.header(print_word("Choose a language"))
+    st.header(translate_word("Choose a language"))
     language_options = ['English','Russian' , 'Hebrew']
     st.session_state.chosen_language = st.selectbox("Choose a language", language_options)
     st.session_state.chosen_language = st.session_state.chosen_language[:2].lower()
