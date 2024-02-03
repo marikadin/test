5e3b1f7a584689b72b4cc47066b44b7d5a660037
marikadin <3007kol@gmail.com>
2024-02-03 18:40:39 +0200

    Update test.py

--- test.py
+++ test.py
@@ -373,8 +373,8 @@ def language_chooser():
         st.session_state.chosen_language = 'en'
 
     st.header(print_word("Choose a language"))
-    language_options = ['russian', 'english', 'hebrew']
-    st.session_state.chosen_language = st.selectbox("Choose a language", language_options, index=language_options.index(st.session_state.chosen_language))
+    language_options = ['Russian', 'English', 'Hebrew']
+    st.session_state.chosen_language = st.selectbox("Choose a language", language_options)
     st.session_state.chosen_language = st.session_state.chosen_language[:2].lower()
 
 
